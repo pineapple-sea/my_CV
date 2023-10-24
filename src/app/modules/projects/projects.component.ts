74cc19b733dbf8de80f9999d716ed0a56dfaa8ab
pinesea <a.dydyluk@gmail.com>
2023-10-24 21:35:49 +0200

    feat: styles;

--- src/app/modules/projects/projects.component.ts
+++ src/app/modules/projects/projects.component.ts
@@ -9,5 +9,21 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
-
+  public projects = [
+    {
+      name: 'Aspiratio',
+      description: 'Social network for timemanagment and motivation',
+      link: 'https://aspiratio.pp.ua/'
+    },
+    {
+      name: 'Ucode IT academy',
+      description: 'implementation gamification and developing parts of the learning-management system',
+      link: null
+    },
+    {
+      name: 'Numuw',
+      description: 'health care system for children with development problems',
+      link: 'https://numuw.com/'
+    }
+  ]
 }
